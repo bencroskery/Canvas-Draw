62e1e24c7652051292b9a68c99f2bf50f717e119
bencroskery <ben@irtech.com>
2016-05-05 22:44:37 -0400

    Initial Setup for new Responsive Design :open_hands:
    Super responsive scaling of elements to keep the canvas in the correct
    aspect but taking advantage of the space in all cases as best as
    possible.

--- src/js/start/start.js
+++ src/js/start/start.js
@@ -3,25 +3,52 @@ import Draw from './draw'
 var canvas = document.getElementById('draw'),
     draw = new Draw(canvas);
 
+let landscape = true;
+
 /**
  * Resize the game window, set the fontSize for CSS sizing, and ask the drawing to resize.
  */
 function resize() {
-    const ASPECT = 16 / 8, PX = "px";
+    const ASPECT = 16 / 9, PX = "px";
     let width = window.innerWidth,
         height = window.innerHeight,
-        gameBox = document.getElementById("game").style;
+        gameBox = document.getElementById("game"),
+        left = document.getElementById("left"),
+        center = document.getElementById("center"),
+        right = document.getElementById("right"),
+        shortest;
 
-    // Scale by largest edge.
-    if (width > height * ASPECT) width = height * ASPECT;
-    else height = width / ASPECT;
 
-    // Set the size and resize the game and drawing canvas.
-    gameBox.width = width + PX;
-    gameBox.height = height + PX;
+    if (width >= height * 1.25 && !landscape) {
+        console.log("switch landscape");
+        gameBox.classList.remove("portrait");
+        gameBox.classList.add("landscape");
+        left.style.height = center.style.height = right.style.height = "";
+        landscape = true;
+    } else if (width < height * 1.25 && landscape) {
+        console.log("switch portrait");
+        gameBox.classList.remove("landscape");
+        gameBox.classList.add("portrait");
+        left.style.width = center.style.width = right.style.width = "";
+        landscape = false;
+    }
+
+    if (landscape) {
+        shortest = height;
+        center.style.width = height * 1.25 + PX;
+        let w = width - height * 1.25;
+        left.style.width = w / 4 + PX;
+        right.style.width = w * 3 / 4 + PX;
+    } else {
+        shortest = width;
+        center.style.height = width * 0.8 + PX;
+        let h = height - width * 0.8;
+        left.style.height = right.style.height = h + PX;
+    }
+
     draw.resize();
 
     // Set the font size based off size and scaling factor (16pt font / 1280px width).
-    document.body.style.fontSize = width / 80 + PX;
+    document.body.style.fontSize = shortest / 45 + PX;
 }
 resize();
