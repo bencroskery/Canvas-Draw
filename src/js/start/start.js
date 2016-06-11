d3656a621a00353ef19afab29bee9a050c125739
bencroskery <ben@irtech.com>
2016-06-11 15:36:03 -0400

    Bottom Flexing
    In portrait the left and right blocks are put into a bottom block to put
    then in a flex row. Adjusted some sizing properties to work better.
    Also fixed package to refer to correct uglifyJS.

--- src/js/start/start.js
+++ src/js/start/start.js
@@ -11,40 +11,45 @@ let landscape = true;
 function resize() {
     const PX = "px";
     let width = window.innerWidth,
-        height = window.innerHeight,
+        height = window.innerHeight - 36,
         gameBox = document.getElementById("game"),
         left = document.getElementById("left"),
         center = document.getElementById("center"),
         right = document.getElementById("right"),
         shortest;
-
-
-    if (width >= height * 1.25 && !landscape) {
+    
+    if (width >= height * 1.30 && !landscape) {
         console.log("switch to landscape");
         // Switch classes.
         gameBox.classList.remove("portrait");
         gameBox.classList.add("landscape");
         landscape = true;
-    } else if (width < height * 1.25 && landscape) {
+
+        gameBox.insertBefore(left, center);
+        gameBox.appendChild(right);
+        gameBox.removeChild(document.getElementById("bottom"));
+    } else if (width < height * 1.30 && landscape) {
         console.log("switch to portrait");
         // Switch classes.
         gameBox.classList.remove("landscape");
         gameBox.classList.add("portrait");
         landscape = false;
+
+        let bottom = document.createElement("div");
+        bottom.id = "bottom";
+        bottom.appendChild(left);
+        bottom.appendChild(right);
+        gameBox.appendChild(bottom);
     }
 
     if (landscape) {
         shortest = height;
+        // Setting flex width in landscape.
         center.style.flexBasis = center.style.msFlexPreferredSize = height * 1.25 + PX;
-        //let w = width - height * 1.25;
-        //left.style.width = w / 4 + PX;
-        //right.style.width = w * 3 / 4 + PX;
     } else {
         shortest = width;
+        // Setting flex height in portrait, subtract menu height.
         center.style.flexBasis = center.style.msFlexPreferredSize = width * 0.8 + PX;
-        // center.style.height = width * 0.8 + PX;
-        // let h = height - width * 0.8;
-        // left.style.height = right.style.height = h + PX;
     }
 
     draw.resize();
