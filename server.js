827498c561fa4db8231bbe5015cf1067a2e22ff5
bencroskery <ben@irtech.com>
2016-06-26 12:55:29 -0400

    GZIP :hammer:
    Added gzip to static assets and replaced socket.io client with CDN for
    compression.
    Also use a gradient for the background because it looks a little nicer.

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 "use strict";
 
 // Setup express server.
-var express = require('express'),
+let express = require('express'),
     app = express(),
+    compress = require('compression'),
     server = require('http').Server(app),
     io = require('socket.io')(server),
     fs = require('fs'),
     _ = require('lodash');
 
 // Folder holding all client pages.
+app.use(compress());
 app.use(express.static(__dirname + '/public'));
 
 // Start listening on the port.
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 server.on('error', (e) => console.log('Another process is already using port ' + port));
 server.listen(port, () => console.log('Server listening on port ' + port));
 
-var players = [],  // List of the names of the users.
+let players = [],  // List of the names of the users.
     running,       // Whether a game is in progress.
     words = [],    // Words the drawer is asked to choose from.
     wordList,      // The full list of all words.
@@ -28,7 +30,7 @@ const MAXSCALER = 1.5;
  * Calculate the skipper to maintain performance while sending points.
  */
 function calcSkip() {
-    skipper = Math.floor((Math.sqrt(players.length-10) || 0)/MAXSCALER);
+    skipper = Math.floor((Math.sqrt(players.length - 10) || 0) / MAXSCALER);
 }
 
 /**
@@ -50,10 +52,10 @@ function generateWords() {
     words = _.sampleSize(wordList, 3);
 }
 
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
     var joined = false;
 
-    socket.on('add user', function (player) {
+    socket.on('add user', (player) => {
         if (running) {
             return;
         }
@@ -70,16 +72,16 @@ io.on('connection', function (socket) {
         calcSkip();
     });
 
-    socket.on('list users', function () {
+    socket.on('list users', () => {
         console.log('Number of players: ' + players.length);
         console.log(players.map(p => p.name));
     });
 
-    socket.on('get skip', function () {
+    socket.on('get skip', () => {
         socket.emit('get skip', skipper);
     });
 
-    socket.on('start game', function () {
+    socket.on('start game', () => {
         if (running) return;
         running = true;
         console.log('Game started!');
@@ -88,41 +90,41 @@ io.on('connection', function (socket) {
         generateWords();
     });
 
-    socket.on('stop game', function () {
+    socket.on('stop game', () => {
         if (!running) return;
         running = false;
         console.log('Game stopped!');
         io.sockets.emit('stop game', 0);
     });
 
-    socket.on('settings', function (s) {
+    socket.on('settings', (s) => {
         if (running) return;
         console.log('Settings changed.');
         socket.broadcast.emit('settings', s);
     });
 
-    socket.on('turn-wait', function (next) {
+    socket.on('turn-wait', (next) => {
         console.log('Next turn');
         io.sockets.emit('turn-wait', next);
         generateWords();
     });
 
-    socket.on('turn-choose', function () {
+    socket.on('turn-choose', () => {
         console.log('Choosing word');
         io.sockets.emit('turn-choose', words);
     });
 
-    socket.on('turn-draw', function (word) {
+    socket.on('turn-draw', (word) => {
         console.log('Drawing');
         if (word === -1) word = _.sample(wordList);
         io.sockets.emit('turn-draw', word);
     });
 
-    socket.on('reveal char', function (i) {
+    socket.on('reveal char', (i) => {
         socket.broadcast.emit('reveal char', i);
     });
 
-    socket.on('p', function (point) {
+    socket.on('p', (point) => {
         if (point.t !== 1) {
             socket.broadcast.emit('p', point);
         } else if (skip === 0) {
@@ -133,25 +135,25 @@ io.on('connection', function (socket) {
         }
     });
 
-    socket.on('set color', function (d) {
+    socket.on('set color', (d) => {
         console.log('Color of layer ' + d.l + ' is now ' + d.c);
         socket.broadcast.emit('set color', d);
     });
 
-    socket.on('set size', function (d) {
+    socket.on('set size', (d) => {
         console.log('Size of layer ' + d.l + ' is now ' + d.r);
         socket.broadcast.emit('set size', d);
     });
 
-    socket.on('undo line', function () {
+    socket.on('undo line', () => {
         socket.broadcast.emit('undo line', 0);
     });
 
-    socket.on('clear canvas', function () {
+    socket.on('clear canvas', () => {
         socket.broadcast.emit('clear canvas', 0);
     });
 
-    socket.on('message', function (msg) {
+    socket.on('message', (msg) => {
         console.log(socket.name + ' said ' + msg);
         // Send the message to everyone.
         socket.broadcast.emit('message', {
@@ -160,19 +162,19 @@ io.on('connection', function (socket) {
         });
     });
 
-    socket.on('correct guess', function () {
+    socket.on('correct guess', () => {
         console.log(socket.name + ' guessed right!');
         // Send the message to everyone.
         io.sockets.emit('correct guess', socket.number);
     });
 
-    socket.on('reboot server', function () {
+    socket.on('reboot server', () => {
         setupGame();
         console.log('Server rebooted.');
         io.sockets.emit('reboot', 0);
     });
 
-    socket.on('disconnect', function () {
+    socket.on('disconnect', () => {
         // Remove the name from global players list.
         if (joined) {
             if (players.length === 1) {
