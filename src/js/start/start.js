72f392208f89b79de259fb2056dd8a597d854f53
bencroskery <ben@irtech.com>
2017-01-18 20:44:03 -0500

    Proper responsive scaling/breakpoints
    - fixes #1
    - added 2 new breakpoints to scale the window when aspect won't fit
    properly
    - reworked the scaling code to be much easier to read
    - renamed classes to represent desktop/mobile aspects

--- src/js/start/start.js
+++ src/js/start/start.js
@@ -1,73 +1,108 @@
 import Draw from './draw'
 
 let canvas = document.getElementById('draw'),
-    draw = new Draw(canvas);
+    draw = new Draw(canvas),
+    resize;
 
-let mode = 2;
+(function () {
+    let mode = 0;
 
-/**
- * Resize the game window, set the fontSize for CSS sizing, and ask the drawing to resize.
- */
-function resize() {
-    const PX = "px";
-    let width = window.innerWidth,
-        height = window.innerHeight - 36,
-        gameBox = document.getElementById("game"),
-        left = document.getElementById("left"),
-        center = document.getElementById("center"),
-        bottom = document.getElementById("bottom"),
-        shortest;
+    /**
+     * Resize the game window, set the fontSize for CSS sizing, and ask the drawing to resize.
+     */
+    resize = function () {
+        const PX = "px";
+        let width = window.innerWidth,
+            height = window.innerHeight - 36,
+            gameBox = document.getElementById("game"),
+            left = document.getElementById("left"),
+            center = document.getElementById("center"),
+            bottom = document.getElementById("bottom"),
+            shortest;
 
-    let asp = width/height;
-    console.log(asp);
+        let aspect = width / height;
 
-    if ((asp < 1.62 && mode === 2) || (asp > 1.30 && mode === 0)) {
-        if (mode === 2) {
+        /*
+
+         0  +     Desktop
+         1  1.62  Squished
+         2  1.48  Scale desktop
+         3  1.00  Scale mobile
+         4  -     Mobile
+
+         */
+
+        // Desktop -> Squished
+        if (aspect < 1.62 && mode === 0) {
             // move left to bottom.
             gameBox.removeChild(left);
             bottom.appendChild(left);
-        } else {
+
+            mode = 1;
+        }
+        // Squished -> Scale desktop
+        if (aspect < 1.48 && mode === 1) {
+            gameBox.classList.add("scaled");
+
+            mode = 2;
+        }
+        // Scale desktop -> Mobile
+        if (aspect < 1.00 && (mode === 2 || mode === 3)) {
             // Switch classes.
-            gameBox.classList.remove("portrait");
-            gameBox.classList.add("landscape");
+            gameBox.classList.add("mobile");
+            gameBox.classList.remove("scaled", "desktop");
+
+            mode = 4;
         }
 
-        mode = 1;
-        console.log("switch to " + mode);
-    }
-    if (asp > 1.62 && mode !== 2) {
-        // move left to gamebox.
-        bottom.removeChild(left);
-        gameBox.appendChild(left);
+        // Mobile -> Scale mobile
+        if (aspect > 1.00 && mode === 4) {
+            gameBox.classList.add("scaled");
 
-        mode = 2;
-        console.log("switch to " + mode);
-    }
-    if (asp < 1.30 && mode !== 0) {
-        // Switch classes.
-        gameBox.classList.remove("landscape");
-        gameBox.classList.add("portrait");
+            mode = 3;
+        }
+        // Scale mobile -> Squished
+        if (aspect > 1.48 && (mode === 3 || mode === 2)) {
+            // Switch classes.
+            gameBox.classList.add("desktop");
+            gameBox.classList.remove("scaled", "mobile");
 
-        mode = 0;
-        console.log("switch to " + mode);
-    }
+            mode = 1;
+        }
+        // Squished -> Desktop
+        if (aspect > 1.62 && mode === 1) {
+            // move left to gamebox.
+            bottom.removeChild(left);
+            gameBox.appendChild(left);
 
-    if (mode != 0) {
-        shortest = height;
-        // Setting flex width in landscape.
-        center.style.flexBasis = center.style.msFlexPreferredSize = height * 1.25 + PX;
-    } else {
-        shortest = width;
-        // Setting flex height in portrait, subtract menu height.
-        center.style.flexBasis = center.style.msFlexPreferredSize = width * 0.8 + PX;
-    }
+            mode = 0;
+        }
 
-    draw.resize();
+        // Fixes for scaled sizes.
+        if (mode === 2) {
+            height = width * 0.675;
+        }
+        if (mode === 3) {
+            width = height;
+        }
 
-    // Set the font size based off size and scaling factor (16pt font / 1280px width).
-    center.style.fontSize = shortest / 45 + PX;
-}
-resize();
+        if (mode < 3) {
+            // Flex width in desktop.
+            center.style.flexBasis = center.style.msFlexPreferredSize = height * 1.25 + PX;
+            shortest = height;
+        } else {
+            // Flex height in mobile.
+            center.style.flexBasis = center.style.msFlexPreferredSize = width * 0.8 + PX;
+            shortest = width;
+        }
 
-// Loaded up, do a spin!
-document.getElementById("logo").classList.add("spin");
+        draw.resize();
+
+        // Set the font size based off size and scaling factor (16pt font / 1280px width).
+        center.style.fontSize = shortest / 45 + PX;
+    };
+    resize();
+
+    // Loaded up, do a spin!
+    document.getElementById("logo").classList.add("spin");
+})();
